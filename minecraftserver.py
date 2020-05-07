4531d9ac464b1130c2b56e5092628e82382c9066
emirhangunec <emirhangunec77@gmail.com>
2020-05-07 19:11:58 +0300

    windows support

--- minecraftserver.py
+++ minecraftserver.py
@@ -4,8 +4,14 @@ import wget
 from time import sleep
 from bs4 import BeautifulSoup
 import requests
-os.system("clear")
+
 defLoc = os.getcwd()
+clear = "clear"
+startText = "start.sh"
+if os.name =="nt":
+    clear = "cls"
+    startText = "start.bat"
+os.system(clear)
 def download(version):
     try:
         soup = BeautifulSoup(requests.get(f"https://mcversions.net/download/{version}").content,"html.parser")
@@ -23,22 +29,25 @@ def download(version):
 def setup(ram):
     if ram.strip().lower().isnumeric():
         try:
-            os.system('echo #!/bin/bash\n > start.sh')
-            os.system(f'echo java -Xmx{ram}M -Xms{ram}M -jar server.jar nogui >> start.sh ')
-            os.system('chmod +x start.sh')
+            os.system(f'echo java -Xmx{ram}M -Xms{ram}M -jar server.jar nogui > {startText}')
+            if not os.name =="nt":
+                os.system('chmod +x start.sh')
             print(f"Starting file created successfully with {ram}MB ram.")
             if not os.path.exists('eula.txt'):
                 print("Editing eula file.")
-                os.system('./start.sh')
+                if not os.name == "nt":
+                    os.system(f'./{startText}')
+                else:
+                    os.system(f'{startText}')
                 sleep(1)
-                os.system('echo eula=true > eula.txt')
+                os.system('echo eula=true> eula.txt')
                 print('Eula edited successfully.')
         except:
             print("Something went wrong")
     else:
         print("Wrong data input, try again.")
 def start():
-    os.system("clear")
+    os.system(clear)
     print("Before run the server, be sure server.properties configured correctly.")
     print("Type 'stop' to stop the server while running.")
     print("Do you want configure the server.properties?")
@@ -46,20 +55,26 @@ def start():
         print("y:yes / n:no")
         editq = input("(y/n): ")
         if editq.strip().lower() == "y":
-            os.system("nano server.properties")
+            if not os.name =="nt":
+                os.system("nano server.properties")
+            else:
+                os.system("notepad server.properties")
             break
         elif editq.strip().lower() == "n":
             print("server.properties file is not edited, continuing.")
             break
         else:
-            os.system("clear")
+            os.system(clear)
             print("Unidintified answer, try again.")
     print("Server starting in 2 sec.")
     sleep(1)
     print("Server starting in 1 sec.")
     sleep(1)
-    os.system("clear")
-    os.system("./start.sh")
+    os.system(clear)
+    if not os.name == "nt":
+        os.system(f'./{startText}')
+    else:
+        os.system(f'{startText}')
 def check(version,location):
     while True:
         selectedLoc = location+"/minecraftServers/"+version
@@ -75,39 +90,38 @@ def check(version,location):
             print("Searching file in path: server.jar")
             if os.path.exists("server.jar"):
                 print("File found: server.jar")
-                print("Searching file in path: start.sh")
-                if os.path.exists("start.sh"):
-                    print("File found: start.sh")
+                print(f"Searching file in path: {startText}")
+                if os.path.exists(startText):
+                    print(f"File found: {startText}")
                     break               
                 else:
-                    print("File not found: start.sh")
+                    print(f"File not found: {startText}")
                     print("Setup is starting.")
                     print("Type ram amount to give server(Default:1024): ")
+                    print("Hit enter to use default.")
                     ramInput = input('MB: ')
                     if ramInput.strip().lower().isnumeric(): 
                         ram = ramInput.strip().lower()
                     else:
                         ram ='1024'
+                    os.chdir(selectedLoc)
                     setup(ram)
                     break
             else:
                 print("File not found: server.jar")
                 print("Searching file in Internet: server.jar")
-                #if not download(version) =="cancel":
                 download(version)
                 print("Setup is starting.")
                 print("Type ram amount to give server(Default:1024): ")
+                print("Hit enter to use default.")
                 ramInput = input('MB: ')
                 if ramInput.strip().lower().isnumeric(): 
                     ram = ramInput.strip().lower()
                 else:
                     ram ='1024'
-                setup(ram)
-                break
-                #else:
-                    # os.rmdir(selectedLoc)
-                    # os.chdir(location)
-                    # return              
+                os.chdir(selectedLoc)
+                setup(ram)      
+    os.chdir(selectedLoc)      
     start()
 print("Welcome to Minecraft Server Manager.")
 print("If you want use defaults, hit enter when asked anything.")
@@ -116,6 +130,7 @@ while True:
     os.chdir(defLoc)
     print("Type 'q' for quit")
     edita = input("Type the version(Default:1.15.2): ")
+    print("Hit enter to use default.")
     if edita.strip().lower() == "q":
         break
     else:
